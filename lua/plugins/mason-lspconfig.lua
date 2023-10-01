78672a9d70807c6d5f9bbf637b5ded3a92781f5c
robertorivera <rrivera@productiveedge.com>
2023-10-01 17:11:52 -0400

    Added and auto install for Mason

---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,24 @@
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "lua_ls",
+    "rust_analyzer",
+    "bashls",
+    "angularls",
+    "astro",
+    "cssls",
+    "cssmodules_ls",
+    "denols",
+    "docker_compose_language_service",
+    "eslint",
+    "emmet_language_server",
+    "html",
+    "biome",
+    "tsserver",
+    "marksman",
+    "intelephense",
+    "prismals",
+    "solidity",
+    "tailwindcss",
+  },
+})
