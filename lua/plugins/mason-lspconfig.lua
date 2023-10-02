3493b8f5cd8254e5864d82e5a19301c4437d286d
Roberto Rivera <rrivera@redbannermedia.com>
2023-10-02 13:36:55 -0400

    Update mason-lspconfig.lua

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,24 +1,28 @@
-require("mason").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = {
-    "lua_ls",
-    "rust_analyzer",
-    "bashls",
-    "angularls",
-    "astro",
-    "cssls",
-    "cssmodules_ls",
-    "denols",
-    "docker_compose_language_service",
-    "eslint",
-    "emmet_language_server",
-    "html",
-    "biome",
-    "tsserver",
-    "marksman",
-    "intelephense",
-    "prismals",
-    "solidity",
-    "tailwindcss",
+return {
+  {
+    "mason-lspconfig",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "rust_analyzer",
+        "bashls",
+        "angularls",
+        "astro",
+        "cssls",
+        "cssmodules_ls",
+        "denols",
+        "docker_compose_language_service",
+        "eslint",
+        "emmet_language_server",
+        "html",
+        "biome",
+        "tsserver",
+        "marksman",
+        "intelephense",
+        "prismals",
+        "solidity",
+        "tailwindcss",
+      },
+    },
   },
-})
+}
