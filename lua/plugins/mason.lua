fd69caa0d02e5583662b9eda405cd634ace77025
Roberto Rivera <rrivera@redbannermedia.com>
2023-11-26 00:18:22 -0500

    updated with mason neo-tree and laravel

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,24 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "tailwindcss-language-server",
+      "svelte-language-server",
+      "deno",
+      "typescript-language-server",
+      "stylua",
+      "php-cs-fixer",
+      "angular-language-server",
+      "lua-language-server",
+      "astro-language-server",
+      "bash-language-server",
+      "blade-formatter",
+      "css-lsp",
+      "json-lsp",
+      "marksman",
+      "phpactor",
+      "shfmt",
+      "solidity-ls",
+    },
+  },
+}
