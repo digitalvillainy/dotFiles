1338a3c9669cb2870b89c60942ffc4db1d0bcbf9
Roberto Rivera <rrivera@redbannermedia.com>
2024-02-15 18:03:49 -0500

    reverted back to phpactor

---
+++ lua/plugins/emmet.lua
@@ -0,0 +1,15 @@
+return {
+  "aca/emmet-ls",
+  filetypes = {
+    "css",
+    "html",
+    "php",
+    "javascript",
+    "less",
+    "sass",
+    "scss",
+    "svelte",
+    "pug",
+    "vue",
+  },
+}
